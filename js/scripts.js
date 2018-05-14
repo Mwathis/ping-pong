a6cd26d8c5ae7a34b3da59c44a204192f10276e2
Charles Mwathi <cmwathi24@gmail.com>
2018-05-14 06:06:54 +0300

    Add more content

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,36 @@
-function fizzBuzz(number){
-	var numbers=[];
-  for (var i=0;i<number;i++){
-   if((i%3===0)&&(i%5===0)){
-      numbers.push("fizzbuzz");
-      }
-    else if(i%3===0){
-      numbers.push("fizz");
-    }
-    else if(i%5===0){
-      numbers.push("buzz");
-    }
-    else{
-    numbers.push(i)
-    }
-  }
-  return numbers;
+// Business Logic
+
+var numbers=[];
+
+function PingPong(number){
+
+		for (var i=1;i<number;i++){
+		 	if(i%15===0){
+				numbers.push("pingpong");
+				}
+			else if(i%5===0){
+				numbers.push("pong");
+			}
+			else if(i%3===0){
+				numbers.push("ping");
+			}
+			else{
+				numbers.push(i)
+			}
+		}
 }
+
+// User Interface
+
+$(document).ready(function(){
+	$("form#ping-pong").submit(function(event){
+		event.preventDefault();
+			var number=parseInt($("input#number").val());
+
+			PingPong(number);
+
+			numbers.forEach(function(number){
+				$("ul#result").append("<li>"+number+"</li>")
+		});
+	});
+});
