11e32bcce7064fc75388f00f76f4e7f710f2df43
Charles Mwathi <cmwathi24@gmail.com>
2018-05-14 12:04:10 +0300

    Change scripts business and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
 // Business Logic
 
-var numbers=[];
-
 function PingPong(number){
-
-		for (var i=1;i<number;i++){
-		 	if(i%15===0){
+	var numbers=[];
+		for (var i=1;i<=number;i++){
+		 	if((i%3===0)&&(i%5===0)){
 				numbers.push("pingpong");
 				}
 			else if(i%5===0){
@@ -18,19 +16,26 @@ function PingPong(number){
 				numbers.push(i)
 			}
 		}
+		return numbers;
 }
 
-// User Interface
+// User-interface logic
 
 $(document).ready(function(){
 	$("form#ping-pong").submit(function(event){
 		event.preventDefault();
+
+			$("ul#output").empty();
 			var number=parseInt($("input#number").val());
 
-			PingPong(number);
-
+			var result=PingPong(number);
+			for(var index=0;index<result.length;index+=1){
+				$("ul#output").append("<li>"+result[index]+"</li>")
+			}
+			/*
 			numbers.forEach(function(number){
-				$("ul#result").append("<li>"+number+"</li>")
+				$("ul#output").append("<li>"+number+"</li>")
 		});
+		*/
 	});
 });
